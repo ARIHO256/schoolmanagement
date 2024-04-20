de5c6b332fafb91437bc0155741a196dcda45d13
ARIHO256 <141637552+ARIHO256@users.noreply.github.com>
2024-04-20 22:54:05 +0300

    e
    we

---
+++ apps/result/utils.py
@@ -0,0 +1,11 @@
+def score_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 75:
+        return "B"
+    elif score >= 60:
+        return "C"
+    elif score >= 50:
+        return "D"
+    else:
+        return "F"
